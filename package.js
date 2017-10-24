c54ad3574a9489667320447ec29a21cb81ff36e3
andrewvanbeek-okta <andrew.vanbeek@okta.com>
2017-10-23 22:15:19 -0500

    new package name

--- package.js
+++ package.js
@@ -1,23 +1,18 @@
 Package.describe({
-  name: 'tunguska:accounts-imgur',
+  name: 'andrewvanbeek-okta:accounts-okta',
   version: '0.0.1',
   summary: 'OAuth2 for Imgur',
-  git: 'https://github.com/robfallows/tunguska-accounts-imgur',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.use(['underscore', 'random']);
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
 
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('tunguska:imgur@0.0.1', ['client', 'server']);
 
-  api.addFiles('accounts-imgur_login_button.css', 'client');
-
-  api.addFiles('accounts-imgur.js');
+  api.use('andrewvanbeek-okta:okta-oauth@1.2.0', ['client', 'server']);
+  api.imply('andrewvanbeek-okta:okta-oauth@1.2.0', ['client', 'server']);
+  api.addFiles('accounts-okta.js');
 });
